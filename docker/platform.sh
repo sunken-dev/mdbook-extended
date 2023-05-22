7229e0b95a0018907b488b3699df14208519caaf
Ch4s3r <lamprecht.patrick1@gmail.com>
2023-05-22 11:09:28 +0200

    done

--- docker/platform.sh
+++ docker/platform.sh
@@ -4,10 +4,10 @@ case $TARGETARCH in
     echo "x86_64-unknown-linux-gnu" > .platform
     echo "gcc-x86-64-linux-gnu" > .compiler
     echo "x86_64-linux-gnu-gcc" > .linker
-	;;
+    ;;
   "arm64")
     echo "aarch64-unknown-linux-gnu" > .platform
     echo "gcc-aarch64-linux-gnu" > .compiler
     echo "aarch64-linux-gnu-gcc" > .linker
-	;;
+    ;;
 esac
